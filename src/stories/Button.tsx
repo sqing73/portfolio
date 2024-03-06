4345f38d131d9310edda8f37fc8b352fd4f1981b
Shan Qing <sqing73@gmail.com>
2024-03-06 11:19:50 -0800

    set up storybook

---
+++ src/stories/Button.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./button.css";
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Button contents
+   */
+  label: string;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+  const [text, setText] = useState(label);
+  return (
+    <button
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+      style={{ backgroundColor }}
+      onClick={() => setText("Clicked?")}
+      {...props}
+    >
+      {text}
+    </button>
+  );
+};
