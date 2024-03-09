f32fdbfb47be885c5323fb834c9585c9da4e194a
Shan Qing <sqing73@gmail.com>
2024-03-08 17:40:51 -0800

    fix eslint error

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import store from "./store";
 import { Provider } from "react-redux";
-import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
